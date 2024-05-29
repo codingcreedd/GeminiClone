0825bbf27ef239ba9eec4bfc76aa5e75a9e7c9b0
codingcreedd <moubayedmarwancoding@gmail.com>
2024-05-29 12:25:10 +0300

    Fix images not showing

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -15,12 +15,29 @@ const Nav = () => {
             </div>
             <div className="flex gap-4 items-center">
                 <div className="hidden sm:flex cursor-pointer hover:bg-purple-400 transition-all gap-1 items-center bg-purple-300 pl-2 py-2 pr-4 rounded-2xl">
-                    <img src="/src/assets/Colorful_Modern_Infinity_Technology_Free_Logo-removebg-preview.png" className="w-[20px] h-[20px]" alt="Miro Logo" />
+                <div
+                        style={{
+                            backgroundImage: `url('https://i.ibb.co/L9WDXh5/Colorful-Modern-Infinity-Technology-Free-Logo-removebg-preview.png')`,
+
+                            backgroundSize: 'contain',
+
+                            height: '50px',
+                            width: '50px',
+                        }}
+                        ></div>
                     <p className="text-[0.8rem] font-bold text-purple-800">Try Miro Advanced</p>
                 </div>
-                <img 
-                className="w-[40px] h-[40px] object-contain border border-purple-800 rounded-full"
-                src="/src/assets/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg" alt="" />
+                <div
+                        style={{
+                            backgroundImage: `url('https://i.ibb.co/fN74KBn/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg')`,
+
+                            backgroundSize: 'contain',
+
+                            height: '40px',
+
+                            width: '40px',
+                        }}
+                        ></div>
             </div>
         </nav>
     )
@@ -65,19 +82,44 @@ const Main = () => {
                 </div>
             ) : (
                 <div className="flex flex-col gap-6 xl:mx-52 md:mx-20 lg:mx-24 mt-14 overflow-auto hiddenScroll">
-                    <div className="flex gap-4 items-center">
-                        <img className="w-[40px] h-[40px] object-contain border border-purple-800 rounded-full"
-                        src="/src/assets/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg" alt="" />
-                        <p>{recentPrompt}</p>
+                    <div className="flex flex-col">
+                        <div className="flex items-center">
+                            <div
+                            style={{
+                                backgroundImage: `url('https://i.ibb.co/fN74KBn/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg')`,
+
+                                backgroundSize: 'contain',
+
+                                height: '40px',
+
+                                width: '40px',
+                            }}
+                            ></div>
+                            <p>User</p>
+                        </div>
+                        <p className="ml-4 text-[0.9rem]">{recentPrompt}</p>
                     </div>
                         
-                    <div className="flex gap-4">
-                        <img src="/src/assets/Colorful_Modern_Infinity_Technology_Free_Logo-removebg-preview.png" className="w-[50px] h-[50px]" alt="Miro Logo" />
+                    <div className="flex flex-col gap-4">
+                        <div className="flex items-center">
+                            <div
+                            style={{
+                                backgroundImage: `url('https://i.ibb.co/L9WDXh5/Colorful-Modern-Infinity-Technology-Free-Logo-removebg-preview.png')`,
+
+                                backgroundSize: 'contain',
+
+                                height: '50px',
+
+                                width: '50px',
+                            }}
+                            ></div>
+                            <p>Miro</p>
+                        </div>
                         {
                             !loading ? (
-                                <div dangerouslySetInnerHTML={{ __html: result }} />
+                                <div className="ml-4 text-[0.9rem]" dangerouslySetInnerHTML={{ __html: result }} />
                             ) : (
-                                <div className="w-[80%]">
+                                <div className="w-[80%] ml-4">
                                     <hr className="mb-4 border-none h-4 rounded-sm" />
                                     <hr className="mb-4 border-none h-4 rounded-sm"/>
                                     <hr className="mb-4 border-none h-4 rounded-sm"/>
